96094d550322721ede070e65046c9e8729ad7624
Lucian Moraes <lucianfmoraes@gmail.com>
2023-03-30 21:25:54 -0300

    section 2 class 21

---
+++ src/main/java/guru/springframework/spring5webapp/controller/BookController.java
@@ -0,0 +1,21 @@
+package guru.springframework.spring5webapp.controller;
+
+import guru.springframework.spring5webapp.repositories.BookRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@AllArgsConstructor
+public class BookController {
+
+    private final BookRepository bookRepository;
+
+    @RequestMapping("/books")
+    public String getBooks(Model model) {
+        model.addAttribute("books", bookRepository.findAll());
+        return "All books";
+    }
+}
